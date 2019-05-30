dd78603f442e5142e9db273e39236bcfeeb09815
Zixuan Jiang <zixuan@utexas.edu>
2019-05-30 15:50:41 -0500

    merge kernels in hpwl_cuda_atomic.cu

--- dreamplace/ops/hpwl/src/hpwl_cuda_atomic_kernel.cu
+++ dreamplace/ops/hpwl/src/hpwl_cuda_atomic_kernel.cu
@@ -8,12 +8,11 @@ DREAMPLACE_BEGIN_NAMESPACE
 
 template <typename T>
 __global__ void computeHPWLMax(
-        const T* x,
-        const int* pin2net_map,
-        const unsigned char* net_mask,
-        int num_pins,
-        T* partial_hpwl_x_max
-        )
+    const T *x,
+    const int *pin2net_map,
+    const unsigned char *net_mask,
+    int num_pins,
+    T *partial_hpwl_x_max)
 {
     int i = blockIdx.x * blockDim.x + threadIdx.x;
     if (i < num_pins)
@@ -22,19 +21,17 @@ __global__ void computeHPWLMax(
         if (net_mask[net_id])
         {
             atomicMax(&partial_hpwl_x_max[net_id], x[i]);
-            __syncthreads();
         }
     }
 }
 
 template <typename T>
 __global__ void computeHPWLMin(
-        const T* x,
-        const int* pin2net_map,
-        const unsigned char* net_mask,
-        int num_pins,
-        T* partial_hpwl_x_min
-        )
+    const T *x,
+    const int *pin2net_map,
+    const unsigned char *net_mask,
+    int num_pins,
+    T *partial_hpwl_x_min)
 {
     int i = blockIdx.x * blockDim.x + threadIdx.x;
     if (i < num_pins)
@@ -43,104 +40,55 @@ __global__ void computeHPWLMin(
         if (net_mask[net_id])
         {
             atomicMin(&partial_hpwl_x_min[net_id], x[i]);
-            __syncthreads();
+        }
+    }
+}
+
+template <typename T>
+__global__ void computeHPWLMaxMin(
+    const T *x, const T *y,
+    const int *pin2net_map,
+    const unsigned char *net_mask,
+    int num_pins,
+    T *partial_hpwl_x_max, T *partial_hpwl_x_min,
+    T *partial_hpwl_y_max, T *partial_hpwl_y_min)
+{
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    if (i < num_pins)
+    {
+        int net_id = pin2net_map[i];
+        if (net_mask[net_id])
+        {
+            atomicMax(&partial_hpwl_x_max[net_id], x[i]);
+            atomicMin(&partial_hpwl_x_min[net_id], x[i]);
+            
+            atomicMax(&partial_hpwl_y_max[net_id], y[i]);
+            atomicMin(&partial_hpwl_y_min[net_id], y[i]);
         }
     }
 }
 
 template <typename T>
 int computeHPWLCudaAtomicLauncher(
-        const T* x, const T* y,
-        const int* pin2net_map,
-        const unsigned char* net_mask,
-        int num_nets,
-        int num_pins,
-        T* partial_hpwl_max,
-        T* partial_hpwl_min
-        )
+    const T *x, const T *y,
+    const int *pin2net_map,
+    const unsigned char *net_mask,
+    int num_nets,
+    int num_pins,
+    T *partial_hpwl_max,
+    T *partial_hpwl_min)
 {
-    const int thread_count = 512;
+    const int thread_count = 64;
     const int block_count_pins = (num_pins + thread_count - 1) / thread_count;
 
-    cudaError_t status;
-    cudaStream_t stream_x_min;
-    status = cudaStreamCreate(&stream_x_min);
-    if (status != cudaSuccess)
-    {
-        printf("cudaStreamCreate failed for stream_x_min\n");
-        fflush(stdout);
-        return 1;
-    }
-    cudaStream_t stream_y_max;
-    status = cudaStreamCreate(&stream_y_max);
-    if (status != cudaSuccess)
-    {
-        printf("cudaStreamCreate failed for stream_y_max\n");
-        fflush(stdout);
-        return 1;
-    }
-    cudaStream_t stream_y_min;
-    status = cudaStreamCreate(&stream_y_min);
-    if (status != cudaSuccess)
-    {
-        printf("cudaStreamCreate failed for stream_y_min\n");
-        fflush(stdout);
-        return 1;
-    }
+    computeHPWLMaxMin<<<block_count_pins, thread_count>>>(
+        x, y,
+        pin2net_map,
+        net_mask,
+        num_pins,
+        partial_hpwl_max, partial_hpwl_min,
+        partial_hpwl_max + num_nets, partial_hpwl_min + num_nets);
 
-    computeHPWLMax<<<block_count_pins, thread_count>>>(
-            x,
-            pin2net_map,
-            net_mask,
-            num_pins,
-            partial_hpwl_max
-            );
-
-    computeHPWLMin<<<block_count_pins, thread_count, 0, stream_x_min>>>(
-            x,
-            pin2net_map,
-            net_mask,
-            num_pins,
-            partial_hpwl_min
-            );
-
-    computeHPWLMax<<<block_count_pins, thread_count, 0, stream_y_max>>>(
-            y,
-            pin2net_map,
-            net_mask,
-            num_pins,
-            partial_hpwl_max+num_nets
-            );
-
-    computeHPWLMin<<<block_count_pins, thread_count, 0, stream_y_min>>>(
-            y,
-            pin2net_map,
-            net_mask,
-            num_pins,
-            partial_hpwl_min+num_nets
-            );
-
-    status = cudaStreamDestroy(stream_x_min);
-    if (status != cudaSuccess)
-    {
-        printf("stream_x_min destroy failed\n");
-        fflush(stdout);
-        return 1;
-    }
-    status = cudaStreamDestroy(stream_y_max);
-    if (status != cudaSuccess)
-    {
-        printf("stream_y_max destroy failed\n");
-        fflush(stdout);
-        return 1;
-    }
-    status = cudaStreamDestroy(stream_y_min);
-    if (status != cudaSuccess)
-    {
-        printf("stream_y_min destroy failed\n");
-        fflush(stdout);
-        return 1;
-    }
     //printArray(partial_hpwl, num_nets, "partial_hpwl");
 
     // I move out the summation to use ATen
@@ -151,25 +99,23 @@ int computeHPWLCudaAtomicLauncher(
 }
 
 // manually instantiate the template function
-#define REGISTER_KERNEL_LAUNCHER(type) \
-    int instantiateComputeHPWLAtomicLauncher(\
-        const type* x, const type* y, \
-        const int* pin2net_map, \
-        const unsigned char* net_mask, \
-        int num_nets, \
-        int num_pins, \
-        type* partial_hpwl_max, \
-        type* partial_hpwl_min \
-        ) \
-    { \
-        return computeHPWLCudaAtomicLauncher(x, y, \
-                pin2net_map, \
-                net_mask, \
-                num_nets, \
-                num_pins, \
-                partial_hpwl_max, \
-                partial_hpwl_min \
-                ); \
+#define REGISTER_KERNEL_LAUNCHER(type)                          \
+    int instantiateComputeHPWLAtomicLauncher(                   \
+        const type *x, const type *y,                           \
+        const int *pin2net_map,                                 \
+        const unsigned char *net_mask,                          \
+        int num_nets,                                           \
+        int num_pins,                                           \
+        type *partial_hpwl_max,                                 \
+        type *partial_hpwl_min)                                 \
+    {                                                           \
+        return computeHPWLCudaAtomicLauncher(x, y,              \
+                                             pin2net_map,       \
+                                             net_mask,          \
+                                             num_nets,          \
+                                             num_pins,          \
+                                             partial_hpwl_max,  \
+                                             partial_hpwl_min); \
     }
 
 REGISTER_KERNEL_LAUNCHER(int);
