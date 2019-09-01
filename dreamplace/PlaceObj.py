fix bug in net weighting; add merged implementation to wa

dreamplace/PlaceObj.py
@@ -74,16 +74,16 @@ class PlaceObj(nn.Module):
         @param pos locations of cells
         @return objective value
         """
-        tt = time.time()
+        #tt = time.time()
         wirelength = self.op_collections.wirelength_op(pos)
-        if self.gpu:
-            torch.cuda.synchronize()
-        print("\t\twirelength forward %.3f ms" % ((time.time()-tt)*1000))
-        tt = time.time()
+        #if self.gpu:
+        #    torch.cuda.synchronize()
+        #print("\t\twirelength forward %.3f ms" % ((time.time()-tt)*1000))
+        #tt = time.time()
         density = self.op_collections.density_op(pos)
-        if self.gpu:
-            torch.cuda.synchronize()
-        print("\t\tdensity forward %.3f ms" % ((time.time()-tt)*1000))
+        #if self.gpu:
+        #    torch.cuda.synchronize()
+        #print("\t\tdensity forward %.3f ms" % ((time.time()-tt)*1000))
         return wirelength + self.density_weight*density
 
     def obj_and_grad_fn(self, pos):
@@ -158,7 +158,7 @@ class PlaceObj(nn.Module):
                 net_mask=data_collections.net_mask_ignore_large_degrees, 
                 pin_mask=data_collections.pin_mask_ignore_fixed_macros,
                 gamma=self.gamma, 
-                algorithm='atomic', 
+                algorithm='merged', 
                 num_threads=params.num_threads
                 )
 

dreamplace/ops/weighted_average_wirelength/weighted_average_wirelength.py
@@ -15,6 +15,7 @@ try:
     import dreamplace.ops.weighted_average_wirelength.weighted_average_wirelength_cuda as weighted_average_wirelength_cuda
     import dreamplace.ops.weighted_average_wirelength.weighted_average_wirelength_cuda_atomic as weighted_average_wirelength_cuda_atomic
     import dreamplace.ops.weighted_average_wirelength.weighted_average_wirelength_cuda_sparse as weighted_average_wirelength_cuda_sparse
+    import dreamplace.ops.weighted_average_wirelength.weighted_average_wirelength_cuda_merged as weighted_average_wirelength_cuda_merged
 except:
     pass
 import pdb
@@ -35,6 +36,7 @@ class WeightedAverageWirelengthFunction(Function):
         @param pin_mask whether compute gradient for a pin, 1 means to fill with zero, 0 means to compute
         @param inv_gamma 1/gamma, the larger, the closer to HPWL
         """
+        tt = time.time()
         if pos.is_cuda:
             output = weighted_average_wirelength_cuda.forward(pos.view(pos.numel()), flat_netpin, netpin_start, pin2net_map, net_weights, net_mask, inv_gamma)
         else:
@@ -55,6 +57,9 @@ class WeightedAverageWirelengthFunction(Function):
         ctx.xyexp_xy_sum = output[5]
         ctx.xyexp_nxy_sum = output[6]
 
+        if pos.is_cuda:
+            torch.cuda.synchronize()
+        print("\t\twirelength forward %.3f ms" % ((time.time()-tt)*1000))
         return output[0]
 
     @staticmethod
@@ -110,7 +115,7 @@ class WeightedAverageWirelengthAtomicFunction(Function):
         @param pin_mask whether compute gradient for a pin, 1 means to fill with zero, 0 means to compute
         @param inv_gamma 1/gamma, the larger, the closer to HPWL
         """
-        #tt = time.time()
+        tt = time.time()
         if pos.is_cuda:
             output = weighted_average_wirelength_cuda_atomic.forward(pos.view(pos.numel()), pin2net_map, flat_netpin, netpin_start, net_weights, net_mask, inv_gamma)
         else:
@@ -131,8 +136,9 @@ class WeightedAverageWirelengthAtomicFunction(Function):
         ctx.pos = pos
         #if torch.isnan(ctx.exp_xy).any() or torch.isnan(ctx.exp_nxy).any() or torch.isnan(ctx.exp_xy_sum).any() or torch.isnan(ctx.exp_nxy_sum).any() or torch.isnan(output[0]).any():
         #    pdb.set_trace()
-        #torch.cuda.synchronize()
-        #print("\t\twirelength forward kernel takes %.3f ms" % ((time.time()-tt)*1000))
+        if pos.is_cuda:
+            torch.cuda.synchronize()
+        print("\t\twirelength forward %.3f ms" % ((time.time()-tt)*1000))
         return output[0]
 
     @staticmethod
@@ -223,6 +229,63 @@ class WeightedAverageWirelengthSparseFunction(Function):
         #print("\t\twirelength backward kernel %.3f ms" % ((time.time()-tt)*1000))
         return output, None, None, None, None, None, None, None, None, None
 
+class WeightedAverageWirelengthMergedFunction(Function):
+    """
+    @brief compute weighted average wirelength.
+    """
+    @staticmethod
+    def forward(ctx, pos, flat_netpin, netpin_start, pin2net_map, net_weights, net_mask, pin_mask, inv_gamma):
+        """
+        @param pos pin location (x array, y array), not cell location
+        @param pin2net_map pin2net map
+        @param net_weights weight of nets
+        @param net_mask whether to compute wirelength
+        @param pin_mask whether compute gradient for a pin, 1 means to fill with zero, 0 means to compute
+        @param inv_gamma 1/gamma, the larger, the closer to HPWL
+        """
+        tt = time.time()
+        if pos.is_cuda:
+            output = weighted_average_wirelength_cuda_merged.forward(pos.view(pos.numel()), flat_netpin, netpin_start, pin2net_map, net_weights, net_mask, inv_gamma)
+        else:
+            assert 0, "CPU version NOT IMPLEMENTED"
+        ctx.pin2net_map = pin2net_map
+        ctx.flat_netpin = flat_netpin
+        ctx.netpin_start = netpin_start
+        ctx.net_weights = net_weights
+        ctx.net_mask = net_mask
+        ctx.pin_mask = pin_mask
+        ctx.inv_gamma = inv_gamma
+        ctx.grad_intermediate = output[1]
+        ctx.pos = pos
+        if pos.is_cuda:
+            torch.cuda.synchronize()
+        print("\t\twirelength forward %.3f ms" % ((time.time()-tt)*1000))
+        return output[0]
+
+    @staticmethod
+    def backward(ctx, grad_pos):
+        tt = time.time()
+        if grad_pos.is_cuda:
+            output = weighted_average_wirelength_cuda_merged.backward(
+                    grad_pos,
+                    ctx.pos,
+                    ctx.grad_intermediate, 
+                    ctx.flat_netpin,
+                    ctx.netpin_start,
+                    ctx.pin2net_map,
+                    ctx.net_weights,
+                    ctx.net_mask,
+                    ctx.inv_gamma
+                    )
+        else:
+            assert 0, "CPU version NOT IMPLEMENTED"
+        output[:int(output.numel()//2)].masked_fill_(ctx.pin_mask, 0.0)
+        output[int(output.numel()//2):].masked_fill_(ctx.pin_mask, 0.0)
+        if grad_pos.is_cuda:
+            torch.cuda.synchronize()
+        print("\t\twirelength backward kernel %.3f ms" % ((time.time()-tt)*1000))
+        return output, None, None, None, None, None, None, None
+
 class WeightedAverageWirelength(nn.Module):
     """
     @brief Compute weighted average wirelength.
@@ -293,6 +356,16 @@ class WeightedAverageWirelength(nn.Module):
                         self.flat_netpin,
                         self.netpin_start,
                         self.netpin_values,
+                        self.pin2net_map,
+                        self.net_weights,
+                        self.net_mask,
+                        self.pin_mask,
+                        1.0/self.gamma # do not store inv_gamma as gamma is changing
+                        )
+            elif self.algorithm == 'merged': 
+                return WeightedAverageWirelengthMergedFunction.apply(pos, 
+                        self.flat_netpin,
+                        self.netpin_start,
                         self.pin2net_map,
                         self.net_weights,
                         self.net_mask,