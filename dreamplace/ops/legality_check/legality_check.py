55dd687fd1e8cf3460aa72f2306be2343920be6d
Yibo Lin <yibolin@utexas.edu>
2020-01-08 22:28:58 +0800

    fix unresolved merge conflicts

--- dreamplace/ops/legality_check/legality_check.py
+++ dreamplace/ops/legality_check/legality_check.py
@@ -11,76 +11,13 @@ from torch.autograd import Function
 
 import dreamplace.ops.legality_check.legality_check_cpp as legality_check_cpp
 
-<<<<<<< HEAD
 class LegalityCheck(object):
-=======
-class LegalityCheckFunction(Function):
->>>>>>> routability
     """ Check legality including, 
     1. out of boundary 
     2. row and site alignment 
     3. overlap 
     4. fence region 
     """
-<<<<<<< HEAD
-=======
-    @staticmethod
-    def forward(
-          pos,
-          node_size_x,
-          node_size_y,
-          flat_region_boxes, 
-          flat_region_boxes_start, 
-          node2fence_region_map, 
-          xl, 
-          yl, 
-          xh, 
-          yh, 
-          site_width, 
-          row_height, 
-          num_terminals, 
-          num_movable_nodes
-          ):
-        if pos.is_cuda:
-            output = greedy_legalize_cpp.forward(
-                    pos.view(pos.numel()).cpu(), 
-                    node_size_x.cpu(),
-                    node_size_y.cpu(),
-                    flat_region_boxes.cpu(), 
-                    flat_region_boxes_start.cpu(), 
-                    node2fence_region_map.cpu(), 
-                    site_width, 
-                    row_height, 
-                    xl, 
-                    yl, 
-                    xh, 
-                    yh, 
-                    num_terminals, 
-                    num_movable_nodes
-                    ).cuda()
-        else:
-            output = greedy_legalize_cpp.forward(
-                    pos.view(pos.numel()), 
-                    node_size_x,
-                    node_size_y,
-                    flat_region_boxes, 
-                    flat_region_boxes_start, 
-                    node2fence_region_map, 
-                    xl, 
-                    yl, 
-                    xh, 
-                    yh, 
-                    site_width, 
-                    row_height, 
-                    num_terminals, 
-                    num_movable_nodes
-                    )
-        return output
-
-class LegalityCheck(object):
-    """ Legalize cells with greedy approach 
-    """
->>>>>>> routability
     def __init__(self, node_size_x, node_size_y, 
             flat_region_boxes, flat_region_boxes_start, node2fence_region_map, 
             xl, yl, xh, yh, site_width, row_height, 
@@ -88,19 +25,11 @@ class LegalityCheck(object):
             num_movable_nodes
             ):
         super(LegalityCheck, self).__init__()
-<<<<<<< HEAD
         self.node_size_x = node_size_x.cpu()
         self.node_size_y = node_size_y.cpu()
         self.flat_region_boxes = flat_region_boxes.cpu() 
         self.flat_region_boxes_start = flat_region_boxes_start.cpu()
         self.node2fence_region_map = node2fence_region_map.cpu()
-=======
-        self.node_size_x = node_size_x
-        self.node_size_y = node_size_y
-        self.flat_region_boxes = flat_region_boxes 
-        self.flat_region_boxes_start = flat_region_boxes_start 
-        self.node2fence_region_map = node2fence_region_map
->>>>>>> routability
         self.xl = xl 
         self.yl = yl
         self.xh = xh 
@@ -117,7 +46,6 @@ class LegalityCheck(object):
         """ 
         @param pos current roughly legal position
         """
-<<<<<<< HEAD
         if pos.is_cuda:
             pos_cpu = pos.cpu()
         else:
@@ -137,21 +65,4 @@ class LegalityCheck(object):
                 self.row_height, 
                 self.num_terminals, 
                 self.num_movable_nodes
-=======
-        return LegalityCheckFunction.forward(
-                pos,
-                node_size_x=self.node_size_x,
-                node_size_y=self.node_size_y,
-                flat_region_boxes=self.flat_region_boxes, 
-                flat_region_boxes_start=self.flat_region_boxes_start, 
-                node2fence_region_map=self.node2fence_region_map, 
-                xl=self.xl, 
-                yl=self.yl, 
-                xh=self.xh, 
-                yh=self.yh, 
-                site_width=self.site_width, 
-                row_height=self.row_height, 
-                num_terminals=self.num_terminals, 
-                num_movable_nodes=self.num_movable_nodes
->>>>>>> routability
                 )
