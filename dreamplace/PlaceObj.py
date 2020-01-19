fix bug in logsumexp wl computation

dreamplace/PlaceObj.py
@@ -205,23 +205,22 @@ class PlaceObj(nn.Module):
         @param data_collections a collection of data and variables required for constructing ops
         @param pin_pos_op the op to compute pin locations according to cell locations
         """
-        gamma = 10*self.base_gamma(params, placedb)
-        logging.info("gamma = %g" % (gamma))
 
         wirelength_for_pin_op = logsumexp_wirelength.LogSumExpWirelength(
                 flat_netpin=data_collections.flat_net2pin_map,
                 netpin_start=data_collections.flat_net2pin_start_map,
                 pin2net_map=data_collections.pin2net_map, 
+                net_weights=data_collections.net_weights, 
                 net_mask=data_collections.net_mask_ignore_large_degrees, 
-                gamma=torch.tensor(gamma, dtype=data_collections.pos[0].dtype, device=data_collections.pos[0].device), 
-                algorithm='atomic', 
+                pin_mask=data_collections.pin_mask_ignore_fixed_macros,
+                gamma=self.gamma, 
+                algorithm='merged', 
                 num_threads=params.num_threads
                 )
 
         # wirelength for position
         def build_wirelength_op(pos):
-            pin_pos = pin_pos_op(pos)
-            return wirelength_for_pin_op(pin_pos)
+            return wirelength_for_pin_op(pin_pos_op(pos))
 
         # update gamma
         base_gamma = self.base_gamma(params, placedb)

dreamplace/ops/logsumexp_wirelength/logsumexp_wirelength.py
@@ -5,83 +5,22 @@
 # @brief  Compute log-sum-exp wirelength and gradient according to NTUPlace3 
 #
 
+import time
 import torch
 from torch import nn
 from torch.autograd import Function
+import logging
 
-import dreamplace.ops.logsumexp_wirelength.logsumexp_wirelength_cpp as logsumexp_wirelength_cpp
+import dreamplace.ops.logsumexp_wirelength.logsumexp_wirelength_cpp_merged as logsumexp_wirelength_cpp_merged
 try: 
-    import dreamplace.ops.logsumexp_wirelength.logsumexp_wirelength_cuda as logsumexp_wirelength_cuda
+    import dreamplace.ops.logsumexp_wirelength.logsumexp_wirelength_cuda_merged as logsumexp_wirelength_cuda_merged
     import dreamplace.ops.logsumexp_wirelength.logsumexp_wirelength_cuda_atomic as logsumexp_wirelength_cuda_atomic
 except:
     pass 
-import pdb 
+import pdb
 
-class LogSumExpWirelengthFunction(Function):
-    """compute weighted average wirelength.
-    @param pos pin location (x array, y array), not cell location 
-    @param flat_netpin flat netpin map, length of #pins 
-    @param netpin_start starting index in netpin map for each net, length of #nets+1, the last entry is #pins  
-    @param pin2net_map pin2net map 
-    @param net_weights weight of nets 
-    @param net_mask whether compute the net or not, 0 as not compute, 1 as compute
-    @param gamma the smaller, the closer to HPWL 
-    """
-    @staticmethod
-    def forward(ctx, pos, flat_netpin, netpin_start, netpin_values, pin2net_map, net_weights, net_mask, gamma, num_threads):
-        if pos.is_cuda:
-            output = logsumexp_wirelength_cuda.forward(pos.view(pos.numel()), flat_netpin, netpin_start, netpin_values, pin2net_map, net_weights, net_mask, gamma)
-        else:
-            output = logsumexp_wirelength_cpp.forward(pos.view(pos.numel()), flat_netpin, netpin_start, net_weights, net_mask, gamma, num_threads)
-        ctx.flat_netpin = flat_netpin
-        ctx.netpin_start = netpin_start
-        ctx.netpin_values = netpin_values
-        ctx.pin2net_map = pin2net_map
-        ctx.net_weights = net_weights
-        ctx.net_mask = net_mask 
-        ctx.gamma = gamma
-        ctx.exp_xy = output[1]
-        ctx.exp_nxy = output[2]
-        ctx.exp_xy_sum = output[3]
-        ctx.exp_nxy_sum = output[4]
-        ctx.num_threads = num_threads
-        ctx.pos = pos 
-        #if torch.isnan(ctx.exp_xy).any() or torch.isnan(ctx.exp_nxy).any() or torch.isnan(ctx.exp_xy_sum).any() or torch.isnan(ctx.exp_nxy_sum).any() or torch.isnan(output[0]).any():
-        #    pdb.set_trace()
-        return output[0]
+logger = logging.getLogger(__name__)
 
-    @staticmethod
-    def backward(ctx, grad_pos):
-        if grad_pos.is_cuda:
-            output = logsumexp_wirelength_cuda.backward(
-                    grad_pos, 
-                    ctx.pos, 
-                    ctx.exp_xy, ctx.exp_nxy, 
-                    ctx.exp_xy_sum, ctx.exp_nxy_sum, 
-                    ctx.flat_netpin, 
-                    ctx.netpin_start, 
-                    ctx.netpin_values, 
-                    ctx.pin2net_map, 
-                    ctx.net_weights, 
-                    ctx.net_mask, 
-                    ctx.gamma
-                    )
-        else:
-            output = logsumexp_wirelength_cpp.backward(
-                    grad_pos, 
-                    ctx.pos, 
-                    ctx.exp_xy, ctx.exp_nxy, 
-                    ctx.exp_xy_sum, ctx.exp_nxy_sum, 
-                    ctx.flat_netpin, 
-                    ctx.netpin_start, 
-                    ctx.net_weights, 
-                    ctx.net_mask, 
-                    ctx.gamma, 
-                    ctx.num_threads
-                    )
-        #if torch.isnan(output).any():
-        #    pdb.set_trace()
-        return output, None, None, None, None, None, None, None, None
 
 class LogSumExpWirelengthAtomicFunction(Function):
     """compute weighted average wirelength.
@@ -129,6 +68,76 @@ class LogSumExpWirelengthAtomicFunction(Function):
         #    pdb.set_trace()
         return output, None, None, None, None
 
+class LogSumExpWirelengthMergedFunction(Function):
+    """
+    @brief compute weighted average wirelength.
+    """
+    @staticmethod
+    def forward(ctx, pos, flat_netpin, netpin_start, pin2net_map, net_weights, net_mask, pin_mask, gamma, num_threads):
+        """
+        @param pos pin location (x array, y array), not cell location
+        @param pin2net_map pin2net map
+        @param net_weights weight of nets
+        @param net_mask whether to compute wirelength
+        @param pin_mask whether compute gradient for a pin, 1 means to fill with zero, 0 means to compute
+        @param gamma the larger, the closer to HPWL
+        """
+        tt = time.time()
+        if pos.is_cuda:
+            output = logsumexp_wirelength_cuda_merged.forward(pos.view(pos.numel()), flat_netpin, netpin_start, pin2net_map, net_weights, net_mask, gamma)
+        else:
+            output = logsumexp_wirelength_cpp_merged.forward(pos.view(pos.numel()), flat_netpin, netpin_start, pin2net_map, net_weights, net_mask, gamma, num_threads)
+            ctx.num_threads = num_threads
+        ctx.pin2net_map = pin2net_map
+        ctx.flat_netpin = flat_netpin
+        ctx.netpin_start = netpin_start
+        ctx.net_weights = net_weights
+        ctx.net_mask = net_mask
+        ctx.pin_mask = pin_mask
+        ctx.gamma = gamma
+        ctx.grad_intermediate = output[1]
+        ctx.pos = pos
+        if pos.is_cuda:
+            torch.cuda.synchronize()
+        logger.debug("wirelength forward %.3f ms" % ((time.time()-tt)*1000))
+        return output[0]
+
+    @staticmethod
+    def backward(ctx, grad_pos):
+        tt = time.time()
+        if grad_pos.is_cuda:
+            output = logsumexp_wirelength_cuda_merged.backward(
+                    grad_pos,
+                    ctx.pos,
+                    ctx.grad_intermediate,
+                    ctx.flat_netpin,
+                    ctx.netpin_start,
+                    ctx.pin2net_map,
+                    ctx.net_weights,
+                    ctx.net_mask,
+                    ctx.gamma
+                    )
+        else:
+            output = logsumexp_wirelength_cpp_merged.backward(
+                    grad_pos,
+                    ctx.pos,
+                    ctx.grad_intermediate,
+                    ctx.flat_netpin,
+                    ctx.netpin_start,
+                    ctx.pin2net_map,
+                    ctx.net_weights,
+                    ctx.net_mask,
+                    ctx.gamma,
+                    ctx.num_threads
+                    )            
+        output[:int(output.numel()//2)].masked_fill_(ctx.pin_mask, 0.0)
+        output[int(output.numel()//2):].masked_fill_(ctx.pin_mask, 0.0)
+        if grad_pos.is_cuda:
+            torch.cuda.synchronize()
+        logger.debug("wirelength backward %.3f ms" % ((time.time()-tt)*1000))
+        return output, None, None, None, None, None, None, None, None
+
+
 class LogSumExpWirelength(nn.Module):
     """ Compute log-sum-exp wirelength. 
     CPU only supports net-by-net algorithm. 
@@ -141,25 +150,25 @@ class LogSumExpWirelength(nn.Module):
     @param net_weights weight of nets 
     @param net_mask whether to compute wirelength, 1 means to compute, 0 means to ignore  
     @param gamma the smaller, the closer to HPWL 
-    @param algorithm must be net-by-net | atomic | sparse 
+    @param algorithm must be merged | atomic
     """
-    def __init__(self, flat_netpin=None, netpin_start=None, pin2net_map=None, net_weights=None, net_mask=None, gamma=None, algorithm='atomic', num_threads=8):
+    def __init__(self, flat_netpin=None, netpin_start=None, pin2net_map=None, net_weights=None, net_mask=None, pin_mask=None, gamma=None, algorithm='merged', num_threads=8):
         super(LogSumExpWirelength, self).__init__()
         assert net_weights is not None \
                 and net_mask is not None \
-                and gamma is not None, "net_weights, net_mask, gamma are requried parameters"
-        if algorithm == 'net-by-net':
-            assert flat_netpin is not None and netpin_start is not None and pin2net_map is not None, "flat_netpin, netpin_start, pin2net_map are requried parameters for algorithm net-by-net"
+                and pin_mask is not None \
+                and gamma is not None, "net_weights, net_mask, pin_mask, gamma are requried parameters"
+        if algorithm == 'merged':
+            assert flat_netpin is not None and netpin_start is not None and pin2net_map is not None, "flat_netpin, netpin_start, pin2net_map are requried parameters for algorithm %s" % (algorithm)
         elif algorithm == 'atomic':
             assert pin2net_map is not None, "pin2net_map is required for algorithm atomic"
-        elif algorithm == 'sparse':
-            assert flat_netpin is not None and netpin_start is not None and pin2net_map is not None, "flat_netpin, netpin_start, pin2net_map are requried parameters for algorithm sparse"
         self.flat_netpin = flat_netpin 
         self.netpin_start = netpin_start
         self.netpin_values = None 
         self.pin2net_map = pin2net_map 
         self.net_weights = net_weights
         self.net_mask = net_mask 
+        self.pin_mask = pin_mask
         self.gamma = gamma
         self.algorithm = algorithm
         self.num_threads = num_threads
@@ -172,27 +181,25 @@ class LogSumExpWirelength(nn.Module):
                         self.net_mask,
                         self.gamma
                         )
-            elif self.algorithm == 'sparse':
-                if self.netpin_values is None: 
-                    self.netpin_values = torch.ones_like(self.flat_netpin, dtype=pos.dtype)
-                return LogSumExpWirelengthFunction.apply(pos, 
-                        self.flat_netpin, 
-                        self.netpin_start, 
-                        self.netpin_values, 
-                        self.pin2net_map, 
-                        self.net_weights, 
+            elif self.algorithm == 'merged':
+                return LogSumExpWirelengthMergedFunction.apply(pos,
+                        self.flat_netpin,
+                        self.netpin_start,
+                        self.pin2net_map,
+                        self.net_weights,
                         self.net_mask,
+                        self.pin_mask,
                         self.gamma, 
                         self.num_threads
                         )
-        else: # only net-by-net for CPU 
-            return LogSumExpWirelengthFunction.apply(pos, 
-                    self.flat_netpin, 
-                    self.netpin_start, 
-                    None, 
-                    self.pin2net_map, 
-                    self.net_weights, 
-                    self.net_mask, 
+        else: # only merged for CPU 
+            return LogSumExpWirelengthMergedFunction.apply(pos, 
+                    self.flat_netpin,
+                    self.netpin_start,
+                    self.pin2net_map,
+                    self.net_weights,
+                    self.net_mask,
+                    self.pin_mask,
                     self.gamma, 
                     self.num_threads
                     )

unitest/ops/logsumexp_wirelength_unitest.py
@@ -63,6 +63,7 @@ def build_wirelength(pin_x, pin_y, pin2net_map, net2pin_map, gamma, ignore_net_d
 
 class LogSumExpWirelengthOpTest(unittest.TestCase):
     def test_logsumexp_wirelength_random(self):
+        dtype = torch.float32
         pin_pos = np.array([[0.0, 0.0], [1.0, 2.0], [1.5, 0.2], [0.5, 3.1], [0.6, 1.1]], dtype=np.float32)*10
         net2pin_map = np.array([np.array([0, 4]), np.array([1, 2, 3])])
         pin2net_map = np.zeros(len(pin_pos), dtype=np.int32)
@@ -75,6 +76,7 @@ class LogSumExpWirelengthOpTest(unittest.TestCase):
         pin_y = pin_pos[:, 1]
         gamma = torch.tensor(0.5, dtype=torch.float32)
         ignore_net_degree = 4
+        pin_mask = np.zeros(len(pin2net_map), dtype=np.uint8)
 
         # net mask 
         net_mask = np.ones(len(net2pin_map), dtype=np.uint8)
@@ -112,13 +114,14 @@ class LogSumExpWirelengthOpTest(unittest.TestCase):
         # clone is very important, because the custom op cannot deep copy the data 
         pin_pos_var.grad.zero_()
         custom = logsumexp_wirelength.LogSumExpWirelength(
-                flat_netpin=torch.from_numpy(flat_net2pin_map), 
-                netpin_start=torch.from_numpy(flat_net2pin_start_map),
-                pin2net_map=torch.from_numpy(pin2net_map), 
-                net_weights=torch.from_numpy(net_weights), 
-                net_mask=torch.from_numpy(net_mask), 
-                gamma=gamma, 
-                algorithm='sparse'
+                flat_netpin=Variable(torch.from_numpy(flat_net2pin_map)),
+                netpin_start=Variable(torch.from_numpy(flat_net2pin_start_map)),
+                pin2net_map=torch.from_numpy(pin2net_map),
+                net_weights=torch.from_numpy(net_weights),
+                net_mask=torch.from_numpy(net_mask),
+                pin_mask=torch.from_numpy(pin_mask),
+                gamma=torch.tensor(gamma, dtype=dtype),
+                algorithm='merged'
                 )
         result = custom.forward(pin_pos_var)
         print("custom = ", result)
@@ -133,13 +136,14 @@ class LogSumExpWirelengthOpTest(unittest.TestCase):
         if torch.cuda.device_count(): 
             pin_pos_var.grad.zero_()
             custom_cuda = logsumexp_wirelength.LogSumExpWirelength(
-                    flat_netpin=Variable(torch.from_numpy(flat_net2pin_map)).cuda(), 
+                    flat_netpin=Variable(torch.from_numpy(flat_net2pin_map)).cuda(),
                     netpin_start=Variable(torch.from_numpy(flat_net2pin_start_map)).cuda(),
-                    pin2net_map=torch.from_numpy(pin2net_map).cuda(), 
-                    net_weights=torch.from_numpy(net_weights).cuda(), 
-                    net_mask=torch.from_numpy(net_mask).cuda(), 
-                    gamma=gamma.cuda(), 
-                    algorithm='sparse'
+                    pin2net_map=torch.from_numpy(pin2net_map).cuda(),
+                    net_weights=torch.from_numpy(net_weights).cuda(),
+                    net_mask=torch.from_numpy(net_mask).cuda(),
+                    pin_mask=torch.from_numpy(pin_mask).cuda(),
+                    gamma=torch.tensor(gamma, dtype=dtype).cuda(),
+                    algorithm='merged'
                     )
             result_cuda = custom_cuda.forward(pin_pos_var.cuda())
             print("custom_cuda_result = ", result_cuda.data.cpu())
@@ -159,6 +163,7 @@ class LogSumExpWirelengthOpTest(unittest.TestCase):
                     pin2net_map=torch.from_numpy(pin2net_map).cuda(), 
                     net_weights=torch.from_numpy(net_weights).cuda(), 
                     net_mask=torch.from_numpy(net_mask).cuda(), 
+                    pin_mask=torch.from_numpy(pin_mask).cuda(),
                     gamma=gamma.cuda(), 
                     algorithm='atomic'
                     )