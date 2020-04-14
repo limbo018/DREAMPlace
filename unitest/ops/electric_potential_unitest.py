d0f98439be7c54fbd8fe1d9bf7a9af3a9ca21c78
Yibo Lin <yibolin@pku.edu.cn>
2020-04-15 00:02:46 +0800

    add deterministic flag to electric potential unittest

--- unitest/ops/electric_potential_unitest.py
+++ unitest/ops/electric_potential_unitest.py
@@ -15,7 +15,9 @@ import os
 import sys
 import gzip
 
-sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+sys.path.append(
+    os.path.dirname(os.path.dirname(os.path.dirname(
+        os.path.abspath(__file__)))))
 from dreamplace.ops.dct import dct
 from dreamplace.ops.dct import discrete_spectral_transform
 from dreamplace.ops.electric_potential import electric_potential
@@ -38,16 +40,50 @@ import matplotlib.pyplot as plt
 class ElectricPotentialOpTest(unittest.TestCase):
     def test_densityOverflowRandom(self):
         dtype = np.float64
-        xx = np.array([1000, 11148 , 11148 , 11148 , 11148 , 11148 , 11124 , 11148 , 11148 , 11137 , 11126 , 11148 , 11130 , 11148 , 11148 , 11148 , 11148 , 11148 , 11148 , 0 , 11148 , 11148 , 11150 , 11134 , 11148 , 11148 , 11148 , 10550 , 11148 , 11148 , 11144 , 11148 , 11148 , 11148 , 11148 , 11140 , 11120 , 11154 , 11148 , 11133 , 11148 , 11148 , 11134 , 11125 , 11148 , 11148 , 11148 , 11155 , 11127 , 11148 , 11148 , 11148 , 11148 , 11131 , 11148 , 11148 , 11148 , 11148 , 11136 , 11148 , 11146 , 11148 , 11135 , 11148 , 11125 , 11150 , 11148 , 11139 , 11148 , 11148 , 11130 , 11148 , 11128 , 11148 , 11138 , 11148 , 11148 , 11148 , 11130 , 11148 , 11132 , 11148 , 11148 , 11090]).astype(dtype)
-        yy = np.array([1000, 11178 , 11178 , 11190 , 11400 , 11178 , 11172 , 11178 , 11178 , 11418 , 11418 , 11178 , 11418 , 11178 , 11178 , 11178 , 11178 , 11178 , 11178 , 11414 , 11178 , 11178 , 11172 , 11418 , 11406 , 11184 , 11178 , 10398 , 11178 , 11178 , 11172 , 11178 , 11178 , 11178 , 11178 , 11418 , 11418 , 11172 , 11178 , 11418 , 11178 , 11178 , 11172 , 11418 , 11178 , 11178 , 11178 , 11418 , 11418 , 11178 , 11178 , 11178 , 11178 , 11418 , 11178 , 11178 , 11394 , 11178 , 11418 , 11178 , 11418 , 11178 , 11418 , 11178 , 11418 , 11418 , 11178 , 11172 , 11178 , 11178 , 11418 , 11178 , 11418 , 11178 , 11418 , 11412 , 11178 , 11178 , 11172 , 11178 , 11418 , 11178 , 11178 , 11414]).astype(dtype)
-        node_size_x = np.array([6, 3 , 3 , 3 , 3 , 3 , 5 , 3 , 3 , 1 , 1 , 3 , 1 , 3 , 3 , 3 , 3 , 3 , 3 , 16728 , 3 , 3 , 5 , 1 , 3 , 3 , 3 , 740 , 3 , 3 , 5 , 3 , 3 , 3 , 3 , 5 , 5 , 5 , 3 , 1 , 3 , 3 , 5 , 1 , 3 , 3 , 3 , 5 , 1 , 3 , 3 , 3 , 3 , 1 , 3 , 3 , 3 , 3 , 5 , 3 , 5 , 3 , 1 , 3 , 5 , 5 , 3 , 5 , 3 , 3 , 5 , 3 , 1 , 3 , 1 , 3 , 3 , 3 , 5 , 3 , 1 , 3 , 3 , 67]).astype(dtype)
-        node_size_y = np.array([6, 240 , 240 , 6 , 6 , 240 , 6 , 240 , 240 , 6 , 6 , 240 , 6 , 240 , 240 , 240 , 240 , 240 , 240 , 10 , 240 , 6 , 6 , 6 , 6 , 6 , 240 , 780 , 240 , 240 , 6 , 240 , 240 , 240 , 240 , 6 , 6 , 6 , 240 , 6 , 240 , 240 , 6 , 6 , 240 , 240 , 240 , 6 , 6 , 240 , 240 , 240 , 240 , 6 , 240 , 240 , 6 , 240 , 6 , 240 , 6 , 240 , 6 , 240 , 6 , 6 , 240 , 6 , 240 , 240 , 6 , 240 , 6 , 240 , 6 , 6 , 240 , 240 , 6 , 240 , 6 , 240 , 240 , 10]).astype(dtype)
+        xx = np.array([
+            1000, 11148, 11148, 11148, 11148, 11148, 11124, 11148, 11148,
+            11137, 11126, 11148, 11130, 11148, 11148, 11148, 11148, 11148,
+            11148, 0, 11148, 11148, 11150, 11134, 11148, 11148, 11148, 10550,
+            11148, 11148, 11144, 11148, 11148, 11148, 11148, 11140, 11120,
+            11154, 11148, 11133, 11148, 11148, 11134, 11125, 11148, 11148,
+            11148, 11155, 11127, 11148, 11148, 11148, 11148, 11131, 11148,
+            11148, 11148, 11148, 11136, 11148, 11146, 11148, 11135, 11148,
+            11125, 11150, 11148, 11139, 11148, 11148, 11130, 11148, 11128,
+            11148, 11138, 11148, 11148, 11148, 11130, 11148, 11132, 11148,
+            11148, 11090
+        ]).astype(dtype)
+        yy = np.array([
+            1000, 11178, 11178, 11190, 11400, 11178, 11172, 11178, 11178,
+            11418, 11418, 11178, 11418, 11178, 11178, 11178, 11178, 11178,
+            11178, 11414, 11178, 11178, 11172, 11418, 11406, 11184, 11178,
+            10398, 11178, 11178, 11172, 11178, 11178, 11178, 11178, 11418,
+            11418, 11172, 11178, 11418, 11178, 11178, 11172, 11418, 11178,
+            11178, 11178, 11418, 11418, 11178, 11178, 11178, 11178, 11418,
+            11178, 11178, 11394, 11178, 11418, 11178, 11418, 11178, 11418,
+            11178, 11418, 11418, 11178, 11172, 11178, 11178, 11418, 11178,
+            11418, 11178, 11418, 11412, 11178, 11178, 11172, 11178, 11418,
+            11178, 11178, 11414
+        ]).astype(dtype)
+        node_size_x = np.array([
+            6, 3, 3, 3, 3, 3, 5, 3, 3, 1, 1, 3, 1, 3, 3, 3, 3, 3, 3, 16728, 3,
+            3, 5, 1, 3, 3, 3, 740, 3, 3, 5, 3, 3, 3, 3, 5, 5, 5, 3, 1, 3, 3, 5,
+            1, 3, 3, 3, 5, 1, 3, 3, 3, 3, 1, 3, 3, 3, 3, 5, 3, 5, 3, 1, 3, 5,
+            5, 3, 5, 3, 3, 5, 3, 1, 3, 1, 3, 3, 3, 5, 3, 1, 3, 3, 67
+        ]).astype(dtype)
+        node_size_y = np.array([
+            6, 240, 240, 6, 6, 240, 6, 240, 240, 6, 6, 240, 6, 240, 240, 240,
+            240, 240, 240, 10, 240, 6, 6, 6, 6, 6, 240, 780, 240, 240, 6, 240,
+            240, 240, 240, 6, 6, 6, 240, 6, 240, 240, 6, 6, 240, 240, 240, 6,
+            6, 240, 240, 240, 240, 6, 240, 240, 6, 240, 6, 240, 6, 240, 6, 240,
+            6, 6, 240, 6, 240, 240, 6, 240, 6, 240, 6, 6, 240, 240, 6, 240, 6,
+            240, 240, 10
+        ]).astype(dtype)
         #xx = np.array([2.0]).astype(dtype)
         #yy = np.array([1.5]).astype(dtype)
         #node_size_x = np.array([1.0]).astype(dtype)
         #node_size_y = np.array([1.0]).astype(dtype)
         num_nodes = len(xx)
-        num_terminals = len(xx)-1
+        num_terminals = len(xx) - 1
 
         scale_factor = 1.0
 
@@ -58,9 +94,8 @@ class ElectricPotentialOpTest(unittest.TestCase):
         target_density = 0.7
         num_bins_x = 1024
         num_bins_y = 1024
-        bin_size_x = (xh-xl)/num_bins_x
-        bin_size_y = (yh-yl)/num_bins_y
-
+        bin_size_x = (xh - xl) / num_bins_x
+        bin_size_y = (yh - yl) / num_bins_y
         """
         return bin xl
         """
@@ -70,28 +105,33 @@ class ElectricPotentialOpTest(unittest.TestCase):
         """
         return bin xh
         """
+
         def bin_xh(id_x):
             return min(bin_xl(id_x) + bin_size_x, xh)
 
         """
         return bin yl
         """
+
         def bin_yl(id_y):
             return yl + id_y * bin_size_y
 
         """
         return bin yh
         """
+
         def bin_yh(id_y):
             return min(bin_yl(id_y) + bin_size_y, yh)
 
         bin_center_x = np.zeros(num_bins_x, dtype=dtype)
         for id_x in range(num_bins_x):
-            bin_center_x[id_x] = (bin_xl(id_x) + bin_xh(id_x)) / 2 * scale_factor
+            bin_center_x[id_x] = (bin_xl(id_x) +
+                                  bin_xh(id_x)) / 2 * scale_factor
 
         bin_center_y = np.zeros(num_bins_y, dtype=dtype)
         for id_y in range(num_bins_y):
-            bin_center_y[id_y] = (bin_yl(id_y) + bin_yh(id_y)) / 2 * scale_factor
+            bin_center_y[id_y] = (bin_yl(id_y) +
+                                  bin_yh(id_y)) / 2 * scale_factor
 
         print("target_area = ", target_density * bin_size_x * bin_size_y)
 
@@ -100,26 +140,34 @@ class ElectricPotentialOpTest(unittest.TestCase):
         elif dtype == np.float32:
             dtype = torch.float32
         movable_size_x = node_size_x[:num_nodes]
-        _, sorted_node_map = torch.sort(torch.tensor(movable_size_x,requires_grad=False, dtype=dtype))
+        _, sorted_node_map = torch.sort(
+            torch.tensor(movable_size_x, requires_grad=False, dtype=dtype))
         sorted_node_map = sorted_node_map.to(torch.int32).contiguous()
         # test cpu
         custom = electric_potential.ElectricPotential(
-            torch.tensor(node_size_x, requires_grad=False, dtype=dtype), torch.tensor(
-                node_size_y, requires_grad=False, dtype=dtype),
-            torch.tensor(bin_center_x, requires_grad=False, dtype=dtype), torch.tensor(
-                bin_center_y, requires_grad=False, dtype=dtype),
-            target_density=torch.tensor(target_density, requires_grad=False, dtype=dtype),
-            xl=xl, yl=yl, xh=xh, yh=yh,
-            bin_size_x=bin_size_x, bin_size_y=bin_size_y,
-            num_movable_nodes=num_nodes-num_terminals,
+            torch.tensor(node_size_x, requires_grad=False, dtype=dtype),
+            torch.tensor(node_size_y, requires_grad=False, dtype=dtype),
+            torch.tensor(bin_center_x, requires_grad=False, dtype=dtype),
+            torch.tensor(bin_center_y, requires_grad=False, dtype=dtype),
+            target_density=torch.tensor(target_density,
+                                        requires_grad=False,
+                                        dtype=dtype),
+            xl=xl,
+            yl=yl,
+            xh=xh,
+            yh=yh,
+            bin_size_x=bin_size_x,
+            bin_size_y=bin_size_y,
+            num_movable_nodes=num_nodes - num_terminals,
             num_terminals=num_terminals,
             num_filler_nodes=0,
             padding=0,
             sorted_node_map=sorted_node_map,
-            movable_macro_mask=None
-        )
+            movable_macro_mask=None,
+            deterministic_flag=True)
 
-        pos = Variable(torch.from_numpy(np.concatenate([xx, yy])), requires_grad=True)
+        pos = Variable(torch.from_numpy(np.concatenate([xx, yy])),
+                       requires_grad=True)
         result = custom.forward(pos)
         print("custom_result = ", result)
         print(result.type())
@@ -130,20 +178,33 @@ class ElectricPotentialOpTest(unittest.TestCase):
         # test cuda
         if torch.cuda.device_count():
             custom_cuda = electric_potential.ElectricPotential(
-                        torch.tensor(node_size_x, requires_grad=False, dtype=dtype).cuda(), torch.tensor(node_size_y, requires_grad=False, dtype=dtype).cuda(),
-                        torch.tensor(bin_center_x, requires_grad=False, dtype=dtype).cuda(), torch.tensor(bin_center_y, requires_grad=False, dtype=dtype).cuda(),
-                        target_density=torch.tensor(target_density, requires_grad=False, dtype=dtype).cuda(),
-                        xl=xl, yl=yl, xh=xh, yh=yh,
-                        bin_size_x=bin_size_x, bin_size_y=bin_size_y,
-                        num_movable_nodes=num_nodes-num_terminals,
-                        num_terminals=num_terminals,
-                        num_filler_nodes=0,
-                        padding=0,
-                        sorted_node_map=sorted_node_map.cuda(), 
-                        movable_macro_mask=None
-                        )
+                torch.tensor(node_size_x, requires_grad=False,
+                             dtype=dtype).cuda(),
+                torch.tensor(node_size_y, requires_grad=False,
+                             dtype=dtype).cuda(),
+                torch.tensor(bin_center_x, requires_grad=False,
+                             dtype=dtype).cuda(),
+                torch.tensor(bin_center_y, requires_grad=False,
+                             dtype=dtype).cuda(),
+                target_density=torch.tensor(target_density,
+                                            requires_grad=False,
+                                            dtype=dtype).cuda(),
+                xl=xl,
+                yl=yl,
+                xh=xh,
+                yh=yh,
+                bin_size_x=bin_size_x,
+                bin_size_y=bin_size_y,
+                num_movable_nodes=num_nodes - num_terminals,
+                num_terminals=num_terminals,
+                num_filler_nodes=0,
+                padding=0,
+                sorted_node_map=sorted_node_map.cuda(),
+                movable_macro_mask=None,
+                deterministic_flag=False)
 
-            pos = Variable(torch.from_numpy(np.concatenate([xx, yy])).cuda(), requires_grad=True)
+            pos = Variable(torch.from_numpy(np.concatenate([xx, yy])).cuda(),
+                           requires_grad=True)
             #pos.grad.zero_()
             result_cuda = custom_cuda.forward(pos)
             print("custom_result_cuda = ", result_cuda.data.cpu())
@@ -152,13 +213,16 @@ class ElectricPotentialOpTest(unittest.TestCase):
             grad_cuda = pos.grad.clone()
             print("custom_grad_cuda = ", grad_cuda.data.cpu())
 
-            #np.testing.assert_allclose(result.detach().numpy(), result_cuda.data.cpu().detach().numpy())
+            np.testing.assert_allclose(result.detach().numpy(),
+                                       result_cuda.data.cpu().detach().numpy())
+
 
 def plot(plot_count, density_map, padding, name):
     """
     density map contour and heat map
     """
-    density_map = density_map[padding:density_map.shape[0] - padding, padding:density_map.shape[1] - padding]
+    density_map = density_map[padding:density_map.shape[0] - padding,
+                              padding:density_map.shape[1] - padding]
     print("max density = %g" % (np.amax(density_map)))
     print("mean density = %g" % (np.mean(density_map)))
 
@@ -203,38 +267,52 @@ def eval_runtime(design):
             f)
 
     dtype = torch.float64
-    num_threads = 10 
+    num_threads = 10
     torch.set_num_threads(num_threads)
     print("num_threads = %d" % (torch.get_num_threads()))
     movable_size_x = node_size_x[:num_movable_nodes]
-    _, sorted_node_map = torch.sort(torch.tensor(movable_size_x,requires_grad=False, dtype=dtype).cuda())
+    _, sorted_node_map = torch.sort(
+        torch.tensor(movable_size_x, requires_grad=False, dtype=dtype).cuda())
     sorted_node_map = sorted_node_map.to(torch.int32).contiguous()
 
-    pos_var = Variable(torch.empty(len(node_size_x) * 2, dtype=dtype).uniform_(xl, xh), requires_grad=True)
+    pos_var = Variable(torch.empty(len(node_size_x) * 2,
+                                   dtype=dtype).uniform_(xl, xh),
+                       requires_grad=True)
     custom = electric_potential.ElectricPotential(
         torch.tensor(node_size_x, requires_grad=False, dtype=dtype).cpu(),
         torch.tensor(node_size_y, requires_grad=False, dtype=dtype).cpu(),
         torch.tensor(bin_center_x, requires_grad=False, dtype=dtype).cpu(),
         torch.tensor(bin_center_y, requires_grad=False, dtype=dtype).cpu(),
-        target_density=torch.tensor(target_density, requires_grad=False, dtype=dtype).cpu(),
-        xl=xl, yl=yl, xh=xh, yh=yh,
-        bin_size_x=bin_size_x, bin_size_y=bin_size_y,
+        target_density=torch.tensor(target_density,
+                                    requires_grad=False,
+                                    dtype=dtype).cpu(),
+        xl=xl,
+        yl=yl,
+        xh=xh,
+        yh=yh,
+        bin_size_x=bin_size_x,
+        bin_size_y=bin_size_y,
         num_movable_nodes=num_movable_nodes,
         num_terminals=num_terminals,
         num_filler_nodes=num_filler_nodes,
         padding=0,
-        sorted_node_map=sorted_node_map.cpu(), 
-        num_threads=num_threads
-        )
+        sorted_node_map=sorted_node_map.cpu(),
+        num_threads=num_threads)
 
     custom_cuda = electric_potential.ElectricPotential(
         torch.tensor(node_size_x, requires_grad=False, dtype=dtype).cuda(),
         torch.tensor(node_size_y, requires_grad=False, dtype=dtype).cuda(),
         torch.tensor(bin_center_x, requires_grad=False, dtype=dtype).cuda(),
         torch.tensor(bin_center_y, requires_grad=False, dtype=dtype).cuda(),
-        target_density=torch.tensor(target_density, requires_grad=False, dtype=dtype).cuda(),
-        xl=xl, yl=yl, xh=xh, yh=yh,
-        bin_size_x=bin_size_x, bin_size_y=bin_size_y,
+        target_density=torch.tensor(target_density,
+                                    requires_grad=False,
+                                    dtype=dtype).cuda(),
+        xl=xl,
+        yl=yl,
+        xh=xh,
+        yh=yh,
+        bin_size_x=bin_size_x,
+        bin_size_y=bin_size_y,
         num_movable_nodes=num_movable_nodes,
         num_terminals=num_terminals,
         num_filler_nodes=num_filler_nodes,
@@ -249,9 +327,10 @@ def eval_runtime(design):
         result = custom.forward(pos_var)
         ttb = time.time()
         result.backward()
-        tbackward += time.time()-ttb
+        tbackward += time.time() - ttb
     torch.cuda.synchronize()
-    print("custom takes %.3f ms, backward %.3f ms" % ((time.time()-tt)/iters*1000, (tbackward/iters*1000)))
+    print("custom takes %.3f ms, backward %.3f ms" %
+          ((time.time() - tt) / iters * 1000, (tbackward / iters * 1000)))
 
     pos_var = pos_var.cuda()
     tt = time.time()
@@ -264,8 +343,10 @@ def eval_runtime(design):
 
 if __name__ == '__main__':
     logging.root.name = 'DREAMPlace'
-    logging.basicConfig(level=logging.DEBUG, format='[%(levelname)-7s] %(name)s - %(message)s', stream=sys.stdout)
-    if len(sys.argv) < 2: 
+    logging.basicConfig(level=logging.DEBUG,
+                        format='[%(levelname)-7s] %(name)s - %(message)s',
+                        stream=sys.stdout)
+    if len(sys.argv) < 2:
         unittest.main()
     else:
         design = sys.argv[1]
