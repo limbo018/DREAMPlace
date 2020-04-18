replace buffer-style density map computation with fixed point numbers

dreamplace/ops/electric_potential/electric_overflow.py
@@ -33,7 +33,6 @@ class ElectricDensityMapFunction(Function):
     @param offset_y (stretched size - node_size) / 2
     @param ratio original area / stretched area 
     @param initial_density_map density_map for fixed cells 
-    @param buf buffer for deterministic density map computation on CPU 
     @param target_density target density 
     @param xl left boundary 
     @param yl lower boundary 
@@ -63,7 +62,6 @@ class ElectricDensityMapFunction(Function):
           ratio,
           bin_center_x, bin_center_y,
           initial_density_map,
-          buf, 
           target_density,
           xl, yl, xh, yh,
           bin_size_x, bin_size_y,
@@ -113,7 +111,6 @@ class ElectricDensityMapFunction(Function):
                     ratio,
                     bin_center_x, bin_center_y,
                     initial_density_map,
-                    buf, 
                     target_density,
                     xl, yl, xh, yh,
                     bin_size_x, bin_size_y,
@@ -126,6 +123,7 @@ class ElectricDensityMapFunction(Function):
                     num_movable_impacted_bins_y,
                     num_filler_impacted_bins_x,
                     num_filler_impacted_bins_y,
+                    deterministic_flag, 
                     num_threads
                     )
 
@@ -174,9 +172,6 @@ class ElectricOverflow(nn.Module):
 
         self.num_threads = num_threads
         self.deterministic_flag = deterministic_flag
-
-        # buffer for deterministic density map computation on CPU 
-        self.buf = torch.Tensor() 
 
         self.reset()
 
@@ -253,12 +248,10 @@ class ElectricOverflow(nn.Module):
                     self.deterministic_flag
                     )
         else:
-            self.buf = torch.empty(self.num_threads * self.num_bins_x * self.num_bins_y, dtype=pos.dtype, device=pos.device)
             self.initial_density_map = electric_potential_cpp.fixed_density_map(
                     pos, 
                     self.node_size_x, self.node_size_y, 
                     self.bin_center_x, self.bin_center_y,
-                    self.buf, 
                     self.xl, self.yl, self.xh, self.yh,
                     self.bin_size_x, self.bin_size_y,
                     self.num_movable_nodes, 
@@ -267,6 +260,7 @@ class ElectricOverflow(nn.Module):
                     self.num_bins_y,
                     num_fixed_impacted_bins_x,
                     num_fixed_impacted_bins_y,
+                    self.deterministic_flag, 
                     self.num_threads
                     )
         # scale density of fixed macros
@@ -283,7 +277,6 @@ class ElectricOverflow(nn.Module):
                 self.ratio,
                 self.bin_center_x, self.bin_center_y,
                 self.initial_density_map,
-                self.buf, 
                 self.target_density,
                 self.xl, self.yl, self.xh, self.yh,
                 self.bin_size_x, self.bin_size_y,