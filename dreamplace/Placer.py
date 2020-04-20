f39c4ef8fb0cf650ea656317d7c2779b19c7cac4
Yibo Lin <yibolin@pku.edu.cn>
2020-04-20 11:46:15 +0800

    add regression test

--- dreamplace/Placer.py
+++ dreamplace/Placer.py
@@ -2,25 +2,26 @@
 # @file   Placer.py
 # @author Yibo Lin
 # @date   Apr 2018
-# @brief  Main file to run the entire placement flow. 
+# @brief  Main file to run the entire placement flow.
 #
 
-import matplotlib 
+import matplotlib
 matplotlib.use('Agg')
 import os
-import sys 
-import time 
-import numpy as np 
+import sys
+import time
+import numpy as np
 import logging
 # for consistency between python2 and python3
 root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if root_dir not in sys.path:
-	sys.path.append(root_dir)
-import dreamplace.configure as configure 
-import Params 
+    sys.path.append(root_dir)
+import dreamplace.configure as configure
+import Params
 import PlaceDB
-import NonLinearPlace 
-import pdb 
+import NonLinearPlace
+import pdb
+
 
 def place(params):
     """
@@ -32,53 +33,68 @@ def place(params):
             "CANNOT enable GPU without CUDA compiled"
 
     np.random.seed(params.random_seed)
-    # read database 
+    # read database
     tt = time.time()
     placedb = PlaceDB.PlaceDB()
     placedb(params)
-    logging.info("reading database takes %.2f seconds" % (time.time()-tt))
+    logging.info("reading database takes %.2f seconds" % (time.time() - tt))
 
-    # solve placement 
+    # solve placement
     tt = time.time()
     placer = NonLinearPlace.NonLinearPlace(params, placedb)
-    logging.info("non-linear placement initialization takes %.2f seconds" % (time.time()-tt))
+    logging.info("non-linear placement initialization takes %.2f seconds" %
+                 (time.time() - tt))
     metrics = placer(params, placedb)
-    logging.info("non-linear placement takes %.2f seconds" % (time.time()-tt))
+    logging.info("non-linear placement takes %.2f seconds" %
+                 (time.time() - tt))
 
-    # write placement solution 
+    # write placement solution
     path = "%s/%s" % (params.result_dir, params.design_name())
     if not os.path.exists(path):
         os.system("mkdir -p %s" % (path))
-    gp_out_file = os.path.join(path, "%s.gp.%s" % (params.design_name(), params.solution_file_suffix()))
+    gp_out_file = os.path.join(
+        path,
+        "%s.gp.%s" % (params.design_name(), params.solution_file_suffix()))
     placedb.write(params, gp_out_file)
 
     # call external detailed placement
-    # TODO: support more external placers, currently only support 
-    # 1. NTUplace3/NTUplace4h with Bookshelf format 
-    # 2. NTUplace_4dr with LEF/DEF format 
-    if params.detailed_place_engine and os.path.exists(params.detailed_place_engine):
-        logging.info("Use external detailed placement engine %s" % (params.detailed_place_engine))
-        if params.solution_file_suffix() == "pl" and any(dp_engine in params.detailed_place_engine for dp_engine in ['ntuplace3', 'ntuplace4h']): 
+    # TODO: support more external placers, currently only support
+    # 1. NTUplace3/NTUplace4h with Bookshelf format
+    # 2. NTUplace_4dr with LEF/DEF format
+    if params.detailed_place_engine and os.path.exists(
+            params.detailed_place_engine):
+        logging.info("Use external detailed placement engine %s" %
+                     (params.detailed_place_engine))
+        if params.solution_file_suffix() == "pl" and any(
+                dp_engine in params.detailed_place_engine
+                for dp_engine in ['ntuplace3', 'ntuplace4h']):
             dp_out_file = gp_out_file.replace(".gp.pl", "")
-            # add target density constraint if provided 
+            # add target density constraint if provided
             target_density_cmd = ""
             if params.target_density < 1.0 and not params.routability_opt_flag:
                 target_density_cmd = " -util %f" % (params.target_density)
-            cmd = "%s -aux %s -loadpl %s %s -out %s -noglobal %s" % (params.detailed_place_engine, params.aux_input, gp_out_file, target_density_cmd, dp_out_file, params.detailed_place_command)
+            cmd = "%s -aux %s -loadpl %s %s -out %s -noglobal %s" % (
+                params.detailed_place_engine, params.aux_input, gp_out_file,
+                target_density_cmd, dp_out_file, params.detailed_place_command)
             logging.info("%s" % (cmd))
             tt = time.time()
             os.system(cmd)
-            logging.info("External detailed placement takes %.2f seconds" % (time.time()-tt))
+            logging.info("External detailed placement takes %.2f seconds" %
+                         (time.time() - tt))
 
-            if params.plot_flag: 
-                # read solution and evaluate 
-                placedb.read_pl(params, dp_out_file+".ntup.pl")
+            if params.plot_flag:
+                # read solution and evaluate
+                placedb.read_pl(params, dp_out_file + ".ntup.pl")
                 iteration = len(metrics)
                 pos = placer.init_pos
                 pos[0:placedb.num_physical_nodes] = placedb.node_x
-                pos[placedb.num_nodes:placedb.num_nodes+placedb.num_physical_nodes] = placedb.node_y
-                hpwl, density_overflow, max_density = placer.validate(placedb, pos, iteration)
-                logging.info("iteration %4d, HPWL %.3E, overflow %.3E, max density %.3E" % (iteration, hpwl, density_overflow, max_density))
+                pos[placedb.num_nodes:placedb.num_nodes +
+                    placedb.num_physical_nodes] = placedb.node_y
+                hpwl, density_overflow, max_density = placer.validate(
+                    placedb, pos, iteration)
+                logging.info(
+                    "iteration %4d, HPWL %.3E, overflow %.3E, max density %.3E"
+                    % (iteration, hpwl, density_overflow, max_density))
                 placer.plot(params, placedb, iteration, pos)
         elif 'ntuplace_4dr' in params.detailed_place_engine:
             dp_out_file = gp_out_file.replace(".gp.def", "")
@@ -91,31 +107,46 @@ def place(params):
             cmd += " -floorplan_def %s" % (gp_out_file)
             cmd += " -verilog %s" % (params.verilog_input)
             cmd += " -out ntuplace_4dr_out"
-            cmd += " -placement_constraints %s/placement.constraints" % (os.path.dirname(params.verilog_input))
+            cmd += " -placement_constraints %s/placement.constraints" % (
+                os.path.dirname(params.verilog_input))
             cmd += " -noglobal %s ; " % (params.detailed_place_command)
-            cmd += "mv ntuplace_4dr_out.fence.plt %s.fense.plt ; " % (dp_out_file)
-            cmd += "mv ntuplace_4dr_out.init.plt %s.init.plt ; " % (dp_out_file)
+            cmd += "mv ntuplace_4dr_out.fence.plt %s.fense.plt ; " % (
+                dp_out_file)
+            cmd += "mv ntuplace_4dr_out.init.plt %s.init.plt ; " % (
+                dp_out_file)
             cmd += "mv ntuplace_4dr_out %s.ntup.def ; " % (dp_out_file)
-            cmd += "mv ntuplace_4dr_out.ntup.overflow.plt %s.ntup.overflow.plt ; " % (dp_out_file)
-            cmd += "mv ntuplace_4dr_out.ntup.plt %s.ntup.plt ; " % (dp_out_file)
+            cmd += "mv ntuplace_4dr_out.ntup.overflow.plt %s.ntup.overflow.plt ; " % (
+                dp_out_file)
+            cmd += "mv ntuplace_4dr_out.ntup.plt %s.ntup.plt ; " % (
+                dp_out_file)
             if os.path.exists("%s/dat" % (os.path.dirname(dp_out_file))):
                 cmd += "rm -r %s/dat ; " % (os.path.dirname(dp_out_file))
             cmd += "mv dat %s/ ; " % (os.path.dirname(dp_out_file))
             logging.info("%s" % (cmd))
             tt = time.time()
             os.system(cmd)
-            logging.info("External detailed placement takes %.2f seconds" % (time.time()-tt))
+            logging.info("External detailed placement takes %.2f seconds" %
+                         (time.time() - tt))
         else:
-            logging.warning("External detailed placement only supports NTUplace3/NTUplace4dr API")
+            logging.warning(
+                "External detailed placement only supports NTUplace3/NTUplace4dr API"
+            )
     elif params.detailed_place_engine:
-        logging.warning("External detailed placement engine %s or aux file NOT found" % (params.detailed_place_engine))
+        logging.warning(
+            "External detailed placement engine %s or aux file NOT found" %
+            (params.detailed_place_engine))
+
+    return metrics
+
 
 if __name__ == "__main__":
     """
     @brief main function to invoke the entire placement flow. 
     """
     logging.root.name = 'DREAMPlace'
-    logging.basicConfig(level=logging.INFO, format='[%(levelname)-7s] %(name)s - %(message)s', stream=sys.stdout)
+    logging.basicConfig(level=logging.INFO,
+                        format='[%(levelname)-7s] %(name)s - %(message)s',
+                        stream=sys.stdout)
     params = Params.Params()
     params.printWelcome()
     if len(sys.argv) == 1 or '-h' in sys.argv[1:] or '--help' in sys.argv[1:]:
@@ -126,13 +157,13 @@ if __name__ == "__main__":
         params.printHelp()
         exit()
 
-    # load parameters 
+    # load parameters
     params.load(sys.argv[1])
     logging.info("parameters = %s" % (params))
     # control numpy multithreading
     os.environ["OMP_NUM_THREADS"] = "%d" % (params.num_threads)
 
-    # run placement 
+    # run placement
     tt = time.time()
     place(params)
-    logging.info("placement takes %.3f seconds" % (time.time()-tt))
+    logging.info("placement takes %.3f seconds" % (time.time() - tt))
