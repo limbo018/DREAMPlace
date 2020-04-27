46a687c7ddbb2f8ffbc54606add951fce390ea2a
JeremieMelo <jqgu1996@163.com>
2020-04-26 23:19:30 -0500

    ispd download and json

--- benchmarks/ispd2019.py
+++ benchmarks/ispd2019.py
@@ -1,11 +1,6 @@
-##
-# @file   ispd2005_2015.py
-# @author Yibo Lin, Zixuan Jiang, Jiaqi Gu
-# @date   Mar 2019
-#
-
 import os
 import sys
+import shutil
 from pyunpack import Archive
 if sys.version_info[0] < 3:
     import urllib2 as urllib
@@ -17,6 +12,11 @@ else:
 baseURL = "http://www.ispd.cc/contests/19/benchmarks/"
 target_dir = os.path.dirname(os.path.abspath(__file__))
 filenames = [f"ispd19_test{i}.tgz" for i in range(1,11)]
+ispd19_dir = os.path.join(target_dir, "ispd2019")
+try:
+    os.mkdir(ispd19_dir)
+except:
+    pass
 
 for filename in filenames:
     file_url = baseURL + filename
@@ -33,3 +33,7 @@ for filename in filenames:
 
     print("remove downloaded file %s" % (path_to_file))
     os.remove(path_to_file)
+    shutil.move(path_to_file[-4], ispd19_dir)
+
+
+
