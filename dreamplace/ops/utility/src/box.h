dbec4cd195058628d261cb9c4600b42e49ef27da
RunzheTao <rztao25@stu.pku.edu.cn>
2025-11-13 21:15:06 +0800

    add CUDA sm_120 support and fix numeric_limits namespace conflict

--- dreamplace/ops/utility/src/box.h
+++ dreamplace/ops/utility/src/box.h
@@ -32,10 +32,10 @@ struct Box {
       : xl(xxl), yl(yyl), xh(xxh), yh(yyh) {}
   /// @brief invalidate the box
   DREAMPLACE_HOST_DEVICE void invalidate() {
-    xl = cuda::numeric_limits<T>::max();
-    yl = cuda::numeric_limits<T>::max();
-    xh = cuda::numeric_limits<T>::lowest();
-    yh = cuda::numeric_limits<T>::lowest();
+    xl = DREAMPLACE_CUDA_NAMESPACE::numeric_limits<T>::max();
+    yl = DREAMPLACE_CUDA_NAMESPACE::numeric_limits<T>::max();
+    xh = DREAMPLACE_CUDA_NAMESPACE::numeric_limits<T>::lowest();
+    yh = DREAMPLACE_CUDA_NAMESPACE::numeric_limits<T>::lowest();
   }
   /// @brief check if the box is valid
   DREAMPLACE_HOST_DEVICE bool valid() const { return (xl <= xh) && (yl <= yh); }
